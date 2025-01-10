1a41a085e2d7626c1365dcc8e613a50a57b8dda4
Manu <professorracecar@gmail.com>
2025-01-09 21:43:24 -0800

    Removed Field trait

--- src/lib.rs
+++ src/lib.rs
@@ -1,74 +1,67 @@
 use num::Num;
 use std::ops;
 
-pub trait Field: Num + Copy {}
-impl<T: Num + Copy> Field for T {}
-
 #[derive(Debug, Copy, Clone, PartialEq)]
-pub struct Matrix<F: Field, const R: usize, const C: usize> ([[F; C]; R]); 
-impl<F: Field, const R: usize, const C: usize> ops::Deref for Matrix<F, R, C> {
+pub struct Matrix<F: Num + Copy, const R: usize, const C: usize> ([[F; C]; R]); 
+impl<F: Num + Copy, const R: usize, const C: usize> ops::Deref for Matrix<F, R, C> {
     type Target = [[F; C]; R];
     fn deref(&self) -> &Self::Target {
         &self.0
     }
 }
-impl<F: Field, const R: usize, const C: usize> ops::DerefMut for Matrix<F, R, C> {
+impl<F: Num + Copy, const R: usize, const C: usize> ops::DerefMut for Matrix<F, R, C> {
     fn deref_mut(&mut self) -> &mut Self::Target {
         &mut self.0
     }
 }
-impl<F: Field, const R: usize, const C: usize> ops::Add<Matrix<F, R, C>> for Matrix<F, R, C> {
+impl<F: Num + Copy, const R: usize, const C: usize> ops::Add<Matrix<F, R, C>> for Matrix<F, R, C> {
     type Output = Matrix<F, R, C>;
 
     fn add(self, right: Matrix<F, R, C>) -> Matrix<F, R, C> {
         madd(self, right)
     }
 }
-impl<F: Field, const R: usize, const C: usize> ops::Sub<Matrix<F, R, C>> for Matrix<F, R, C> {
+impl<F: Num + Copy, const R: usize, const C: usize> ops::Sub<Matrix<F, R, C>> for Matrix<F, R, C> {
     type Output = Matrix<F, R, C>;
 
     fn sub(self, right: Matrix<F, R, C>) -> Matrix<F, R, C> {
         msub(self, right)
     }
 }
-impl<F: Field, const R: usize, const C: usize> ops::Mul<F> for Matrix<F, R, C> {
+impl<F: Num + Copy, const R: usize, const C: usize> ops::Mul<F> for Matrix<F, R, C> {
     type Output = Matrix<F, R, C>;
 
     fn mul(self, left: F) -> Matrix<F, R, C> {
         mscale(left, self)
     }
 }
-impl<F: Field, const R: usize, const C: usize> ops::Div<F> for Matrix<F, R, C> {
+impl<F: Num + Copy, const R: usize, const C: usize> ops::Div<F> for Matrix<F, R, C> {
     type Output = Matrix<F, R, C>;
 
     fn div(self, left: F) -> Matrix<F, R, C> {
         mdiv(left, self)
     }
 }
-impl<F: Field, const L: usize, const M: usize, const N: usize> ops::Mul<Matrix<F, M, N>> for Matrix<F, L, M> {
+impl<F: Num + Copy, const L: usize, const M: usize, const N: usize> ops::Mul<Matrix<F, M, N>> for Matrix<F, L, M> {
     type Output = Matrix<F, L, N>;
 
     fn mul(self, right: Matrix<F, M, N>) -> Matrix<F, L, N> {
         mmul(self, right)
     }
 }
-impl<F: Field, const R: usize, const C: usize> ops::Mul<Vector<F, C>> for Matrix<F, R, C> {
+impl<F: Num + Copy, const R: usize, const C: usize> ops::Mul<Vector<F, C>> for Matrix<F, R, C> {
     type Output = Vector<F, R>;
 
     fn mul(self, right: Vector<F, C>) -> Vector<F, R> {
         vmul(self, right)
     }
 }
-impl<F: Field, const R: usize, const C: usize> Matrix<F, R, C> {
+impl<F: Num + Copy, const R: usize, const C: usize> Matrix<F, R, C> {
     pub fn from(array: [[F; C]; R]) -> Matrix<F, R, C> {
         Matrix(array)
     }
 
     pub fn to_array(&self) -> [[F; C]; R] {
-        self.0
-    }
-
-    pub fn as_array(&mut self) -> [[F; C]; R] {
         self.0
     }
 
@@ -197,63 +190,59 @@ impl<F: Field, const R: usize, const C: usize> Matrix<F, R, C> {
 }
 
 #[derive(Debug, Copy, Clone, PartialEq)]
-pub struct Vector<F: Field, const R: usize> ([F; R]);
-impl<F: Field, const R: usize> ops::Deref for Vector<F, R> {
+pub struct Vector<F: Num + Copy, const R: usize> ([F; R]);
+impl<F: Num + Copy, const R: usize> ops::Deref for Vector<F, R> {
     type Target = [F; R];
     fn deref(&self) -> &Self::Target {
         &self.0
     }
 }
-impl<F: Field, const R: usize> ops::DerefMut for Vector<F, R> {
+impl<F: Num + Copy, const R: usize> ops::DerefMut for Vector<F, R> {
     fn deref_mut(&mut self) -> &mut Self::Target {
         &mut self.0
     }
 }
-impl<F: Field, const R: usize> ops::Add<Vector<F, R>> for Vector<F, R> {
+impl<F: Num + Copy, const R: usize> ops::Add<Vector<F, R>> for Vector<F, R> {
     type Output = Vector<F, R>;
 
     fn add(self, right: Vector<F, R>) -> Vector<F, R> {
         vadd(self, right)
     }
 }
-impl<F: Field, const R: usize> ops::Sub<Vector<F, R>> for Vector<F, R> {
+impl<F: Num + Copy, const R: usize> ops::Sub<Vector<F, R>> for Vector<F, R> {
     type Output = Vector<F, R>;
 
     fn sub(self, right: Vector<F, R>) -> Vector<F, R> {
         vsub(self, right)
     }
 }
-impl<F: Field, const R: usize> ops::Mul<F> for Vector<F, R> {
+impl<F: Num + Copy, const R: usize> ops::Mul<F> for Vector<F, R> {
     type Output = Vector<F, R>;
 
     fn mul(self, left: F) -> Vector<F, R> {
         vscale(left, self)
     }
 }
-impl<F: Field, const R: usize> ops::Div<F> for Vector<F, R> {
+impl<F: Num + Copy, const R: usize> ops::Div<F> for Vector<F, R> {
     type Output = Vector<F, R>;
 
     fn div(self, left: F) -> Vector<F, R> {
         vdiv(left, self)
     }
 }
-impl<F: Field, const R: usize> ops::Mul<Vector<F, R>> for Vector<F, R> {
+impl<F: Num + Copy, const R: usize> ops::Mul<Vector<F, R>> for Vector<F, R> {
     type Output = F;
 
     fn mul(self, right: Vector<F, R>) -> F {
         dot(self, right)
     }
 }
-impl<F: Field, const R: usize> Vector<F, R> {
+impl<F: Num + Copy, const R: usize> Vector<F, R> {
     pub fn from(array: [F; R]) -> Vector<F, R> {
         Vector(array)
     }
 
     pub fn to_array(&self) -> [F; R] {
-        self.0
-    }
-
-    pub fn as_array(&mut self) -> [F; R] {
         self.0
     }
 
@@ -268,7 +257,7 @@ impl<F: Field, const R: usize> Vector<F, R> {
 }
 
 // Functions 
-pub fn vscale<F: Field, const R: usize>(scalar: F, vector: Vector<F, R>) -> Vector<F, R> { 
+pub fn vscale<F: Num + Copy, const R: usize>(scalar: F, vector: Vector<F, R>) -> Vector<F, R> { 
     let mut result: [F; R] = [F::zero(); R];
     for i in 0..R {
         result[i] = scalar * vector[i];
@@ -277,7 +266,7 @@ pub fn vscale<F: Field, const R: usize>(scalar: F, vector: Vector<F, R>) -> Vect
     Vector::from(result)
 }
 
-pub fn vdiv<F: Field, const R: usize>(scalar: F, vector: Vector<F, R>) -> Vector<F, R> { 
+pub fn vdiv<F: Num + Copy, const R: usize>(scalar: F, vector: Vector<F, R>) -> Vector<F, R> { 
     let mut result: [F; R] = [F::zero(); R];
     for i in 0..R {
         result[i] = vector[i] / scalar;
@@ -286,7 +275,7 @@ pub fn vdiv<F: Field, const R: usize>(scalar: F, vector: Vector<F, R>) -> Vector
     Vector::from(result)
 }
 
-pub fn mscale<F: Field, const R: usize, const C: usize>(scalar: F, matrix: Matrix<F, R, C>) -> Matrix<F, R, C> { 
+pub fn mscale<F: Num + Copy, const R: usize, const C: usize>(scalar: F, matrix: Matrix<F, R, C>) -> Matrix<F, R, C> { 
     let mut result: [[F; C]; R] = [[F::zero(); C]; R];
     for i in 0..R {
         for j in 0..C {
@@ -297,7 +286,7 @@ pub fn mscale<F: Field, const R: usize, const C: usize>(scalar: F, matrix: Matri
     Matrix::from(result)
 }
 
-pub fn mdiv<F: Field, const R: usize, const C: usize>(scalar: F, matrix: Matrix<F, R, C>) -> Matrix<F, R, C> { 
+pub fn mdiv<F: Num + Copy, const R: usize, const C: usize>(scalar: F, matrix: Matrix<F, R, C>) -> Matrix<F, R, C> { 
     let mut result: [[F; C]; R] = [[F::zero(); C]; R];
     for i in 0..R {
         for j in 0..C {
@@ -308,7 +297,7 @@ pub fn mdiv<F: Field, const R: usize, const C: usize>(scalar: F, matrix: Matrix<
     Matrix::from(result)
 }
 
-pub fn vadd<F: Field, const R: usize>(left: Vector<F, R>, right: Vector<F, R>) -> Vector<F, R> { 
+pub fn vadd<F: Num + Copy, const R: usize>(left: Vector<F, R>, right: Vector<F, R>) -> Vector<F, R> { 
     let mut result: [F; R] = [F::zero(); R];
     for i in 0..R {
         result[i] = left[i] + right[i];
@@ -317,7 +306,7 @@ pub fn vadd<F: Field, const R: usize>(left: Vector<F, R>, right: Vector<F, R>) -
     Vector::from(result)
 }
 
-pub fn vsub<F: Field, const R: usize>(left: Vector<F, R>, right: Vector<F, R>) -> Vector<F, R> { 
+pub fn vsub<F: Num + Copy, const R: usize>(left: Vector<F, R>, right: Vector<F, R>) -> Vector<F, R> { 
     let mut result: [F; R] = [F::zero(); R];
     for i in 0..R {
         result[i] = left[i] - right[i];
@@ -326,7 +315,7 @@ pub fn vsub<F: Field, const R: usize>(left: Vector<F, R>, right: Vector<F, R>) -
     Vector::from(result)
 }
 
-pub fn madd<F: Field, const R: usize, const C: usize>(left: Matrix<F, R, C>, right: Matrix<F, R, C>) -> Matrix<F, R, C> { 
+pub fn madd<F: Num + Copy, const R: usize, const C: usize>(left: Matrix<F, R, C>, right: Matrix<F, R, C>) -> Matrix<F, R, C> { 
     let mut result: [[F; C]; R] = [[F::zero(); C]; R];
     for i in 0..R {
         for j in 0..C {
@@ -337,7 +326,7 @@ pub fn madd<F: Field, const R: usize, const C: usize>(left: Matrix<F, R, C>, rig
     Matrix::from(result)
 }
 
-pub fn msub<F: Field, const R: usize, const C: usize>(left: Matrix<F, R, C>, right: Matrix<F, R, C>) -> Matrix<F, R, C> { 
+pub fn msub<F: Num + Copy, const R: usize, const C: usize>(left: Matrix<F, R, C>, right: Matrix<F, R, C>) -> Matrix<F, R, C> { 
     let mut result: [[F; C]; R] = [[F::zero(); C]; R];
     for i in 0..R {
         for j in 0..C {
@@ -348,7 +337,7 @@ pub fn msub<F: Field, const R: usize, const C: usize>(left: Matrix<F, R, C>, rig
     Matrix::from(result)
 }
 
-pub fn dot<F: Field, const R: usize>(left: Vector<F, R>, right: Vector<F, R>) -> F {
+pub fn dot<F: Num + Copy, const R: usize>(left: Vector<F, R>, right: Vector<F, R>) -> F {
     let mut product: F = F::zero();
     for i in 0..R {
         product = product + (left[i] * right[i]);
@@ -357,7 +346,7 @@ pub fn dot<F: Field, const R: usize>(left: Vector<F, R>, right: Vector<F, R>) ->
     product
 }
 
-pub fn mmul<F: Field, const L: usize, const M: usize, const N: usize>(left: Matrix<F, L, M>, right: Matrix<F, M, N>) -> Matrix<F, L, N> { // try strassen for large?
+pub fn mmul<F: Num + Copy, const L: usize, const M: usize, const N: usize>(left: Matrix<F, L, M>, right: Matrix<F, M, N>) -> Matrix<F, L, N> { // try strassen for large?
     let mut result:[[F; N]; L] = [[F::zero(); N]; L];
     for i in 0..N {
         for j in 0..L {
@@ -368,7 +357,7 @@ pub fn mmul<F: Field, const L: usize, const M: usize, const N: usize>(left: Matr
     Matrix::from(result)
 }
 
-pub fn vmul<F: Field, const R: usize, const C: usize>(left: Matrix<F, R, C>, right: Vector<F, C>) -> Vector<F, R> {
+pub fn vmul<F: Num + Copy, const R: usize, const C: usize>(left: Matrix<F, R, C>, right: Vector<F, C>) -> Vector<F, R> {
     let mut result:[F; R]  = [F::zero(); R];
     for i in 0..R {
         result[i] = dot(left.get_row(i), right);
@@ -377,7 +366,7 @@ pub fn vmul<F: Field, const R: usize, const C: usize>(left: Matrix<F, R, C>, rig
     Vector::from(result)
 }
 
-pub fn identity<F: Field, const D: usize> () -> Matrix<F, D, D> {
+pub fn identity<F: Num + Copy, const D: usize> () -> Matrix<F, D, D> {
     let mut result: [[F; D]; D] = [[F::zero(); D]; D];
     for i in 0..D {
         result[i][i] = F::one();
@@ -386,7 +375,7 @@ pub fn identity<F: Field, const D: usize> () -> Matrix<F, D, D> {
     Matrix::from(result)
 }
 
-pub fn zero<F: Field, const D:usize> () -> Matrix<F, D, D> {
+pub fn zero<F: Num + Copy, const D:usize> () -> Matrix<F, D, D> {
     Matrix::from([[F::zero(); D]; D])
 }
 
